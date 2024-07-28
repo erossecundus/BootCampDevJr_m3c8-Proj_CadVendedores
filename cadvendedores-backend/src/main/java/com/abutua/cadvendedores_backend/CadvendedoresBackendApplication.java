3a1efc8eb9d31f7a9d0fb93b755a61e8d0f95846
Eros Secundus <erossecundus@gmail.com>
2024-07-28 01:16:16 -0300

    closes #2 - first commit; create springboot project

---
+++ cadvendedores-backend/src/main/java/com/abutua/cadvendedores_backend/CadvendedoresBackendApplication.java
@@ -0,0 +1,13 @@
+package com.abutua.cadvendedores_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadvendedoresBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadvendedoresBackendApplication.class, args);
+	}
+
+}
---
+++ cadvendedores-backend/src/test/java/com/abutua/cadvendedores_backend/CadvendedoresBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abutua.cadvendedores_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadvendedoresBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

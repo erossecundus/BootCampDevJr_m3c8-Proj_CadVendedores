b8d29cea0b20aa387c5b73384d1e3a828ab9b9a9
Eros Secundus <erossecundus@gmail.com>
2024-07-30 20:54:43 -0300

    closes #12 - Botões: Alterar e Remover

--- cadvendedores-frontend/src/app/components/sellers/sellers.component.ts
+++ cadvendedores-frontend/src/app/components/sellers/sellers.component.ts
@@ -11,6 +11,8 @@ export class SellersComponent {
   title = 'Vendedores Cadastrados';
 
   seller: Seller = {} as Seller;
+  isUpdate: Boolean = false;
+  idCount: number = 3;
 
   genders: string[] = [
     "Masculino",
@@ -19,17 +21,30 @@ export class SellersComponent {
   ];
 
   sellers : Seller[] = [
-    {id:1, name:"João", salary:1500.50, bonus:0.10, gender:1},
-    {id:2, name:"Maria", salary:1800.50, bonus:0.2055, gender:2},
-    {id:3, name:"Darcy", salary:2000.50, bonus:0.255, gender:3}
+    {id:1, name:"João", salary:1500.50, bonus:10, gender:1},
+    {id:2, name:"Maria", salary:1800.50, bonus:20.55, gender:2},
+    {id:3, name:"Darcy", salary:2000.50, bonus:25.5, gender:3}
   ];
 
-  saveBook() {
-    this.seller.id = this.sellers.length+1;
-    this.seller.bonus /= 100; 
-    this.sellers.push(this.seller);
+  saveSeller() {
+    if (!this.isUpdate) {
+      this.seller.id = this.idCount+1;
+      this.sellers.push(this.seller);
+      this.idCount++;
+    } 
     this.seller = {} as Seller;
-  }
+    this.isUpdate = false;
+
+  };
+
+  updateSeller(selectedSeller: Seller) {
+    this.isUpdate = true;
+    this.seller = selectedSeller;
+  };
+
+  removeSeller(selectedSeller: Seller) {
+    this.sellers = this.sellers.filter(s => s.id !== selectedSeller.id);
+  };
 
 
 
